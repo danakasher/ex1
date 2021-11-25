2f66880218904741358fe9572a9fdb22c9dccdce
Asaf Kovrovski <35013356+AsafKov@users.noreply.github.com>
2021-11-25 09:16:24 +0200

    Working AVL

--- Node.h
+++ Node.h
@@ -1,18 +1,20 @@
 #ifndef EX1_NODE_H
 #define EX1_NODE_H
 
-template <typename T> class Node {
+template <typename Data, typename Key>
+class Node {
 private:
-    T value; //TODO: shared ptr?
-    Node<T>* left;
-    Node<T>* right;
-    Node<T>* father;
+    Data value;
+    Key key;
+    Node<Data, Key>* left;
+    Node<Data,Key>* right;
+    Node<Data,Key>* father;
 
 public:
     int height, balancingParameter;
-    explicit Node<T>(T const &val) : value(val), left(nullptr), right(nullptr), father(nullptr), height(0), balancingParameter(0) {};
+    explicit Node<Data,Key>(Data const &val) : value(val), left(nullptr), right(nullptr), father(nullptr), height(0), balancingParameter(0) {};
     ~Node() = default;
-    T &getValue(){ return this->value; }
+    Data &getValue(){ return this->value; }
     Node *getLeft(){ return this->left; }
     Node *getRight(){ return this->right; }
     void setLeft(Node *leftNode){
@@ -21,20 +23,20 @@ public:
     void setRight(Node *rightNode){
         this->right = rightNode;
     }
-    bool operator>(Node<T> &node) const {
+    bool operator>(Node<Data,Key> &node) const {
         return this->value > (node->getValue());
     }
 
-    bool operator==(Node<T> &node) const{
+    bool operator==(Node<Data,Key> &node) const{
         return this->value == node->value;
     }
-    bool operator>=(Node<T> &node) const{
+    bool operator>=(Node<Data,Key> &node) const{
         return  this ==node || this > node;
     }
 
-    void setFather(Node<T> *father){ this->father = father;}
+    void setFather(Node<Data,Key> *prev){ this->father = prev;}
 
-    Node<T> *getFather(){ return this->father; }
+    Node<Data,Key> *getFather(){ return this->father; }
 };
 
 
