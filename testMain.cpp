f95f308d4976fa6f7836cc4a4f3843c7dd3f1cad
Asaf Kovrovski <35013356+AsafKov@users.noreply.github.com>
2021-12-01 22:16:40 +0200

    Current working version

--- testMain.cpp
+++ testMain.cpp
@@ -90,7 +90,7 @@ static bool isInit = false;
 /***************************************************************************/
 
 int main(int argc, const char**argv) {
-    freopen("output7.txt","w",stdout);
+    freopen("o1.txt","w",stdout);
     char buffer[MAX_STRING_INPUT_SIZE];
 
     // Reading commands
